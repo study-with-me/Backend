f2c7f76aa55b6b5777d4034cc4565fa6be6b1c57
mcasertano <43253734+mcasertano@users.noreply.github.com>
2019-05-11 16:15:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-"" 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send('test');
+});
+
+app.listen(3000, () => console.log('server started'));
