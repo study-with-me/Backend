f36e35e22e627890ccc6f1ca39fa1310b96477d7
Sesamestrong <doolie.healey@gmail.com>
2019-05-11 16:36:18 -0400

    Remove reference to missing db.js file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const db = require('./db');
+//const db = require('./db');
 
 const app = express();
 
